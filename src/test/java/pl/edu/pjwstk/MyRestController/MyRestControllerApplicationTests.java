a3f58b4c06be3c581eca1573650810666dbe0866
“Denys <denn8051@gmail.com>
2024-10-07 09:29:00 +0200

    first commit

---
+++ src/test/java/pl/edu/pjwstk/MyRestController/MyRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.MyRestController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
